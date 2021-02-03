0dcaa5445b4bf11935569d3208471deb969d96da
Denis Kolodich <denis.k.98@mail.ru>
2021-02-03 22:07:36 +0300

    Add solution

--- 04_lab/main.cpp
+++ 04_lab/main.cpp
@@ -4,6 +4,13 @@
 // После чего, каждую цифру этого числа необходимо вывести в новой строке.
 
 int main() {
-    // Решение тут
+    int num=0;
+    std::cout<<"Enter number:";
+    std::cin>>num;
+    while(num>0)
+    {
+        std::cout<<num%10<<std::endl;
+        num/=10;
+    }
     return 0;
 }
