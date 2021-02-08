511dc00dc106829e2289d31a57a9e1cdcfd20f95
Andrey Zykov <andrei-z30@yandex.ru>
2021-02-08 17:45:05 +0300

    Add_lab_05

--- 05_lab/main.cpp
+++ 05_lab/main.cpp
@@ -4,6 +4,12 @@
 // N вводится с клавиатуры. Вывести сумму.
 
 int main() {
-    // Решение тут
+    std::cout << "Sum numbers:" << std::endl;
+    int n, result = 0;
+    std::cin >> n;
+    for (int i = 1; i <= n; i++) {
+        result += i;
+    }
+    std::cout << "Result: " << result;
     return 0;
 }
