8d97ab53ef8150c927686d073912d564dc71f4f4
Baranovskij <70894805+Baranovskij@users.noreply.github.com>
2021-02-08 16:52:54 +0300

    Create main.cpp
    solution

--- 04_lab/main.cpp
+++ 04_lab/main.cpp
@@ -5,5 +5,20 @@
 
 int main() {
     // Решение тут
+     int n, a, b, c, d, e;
+    std::cin >> n;
+    a = (n/10000) - ((n%10000)/10000);
+    n = n - a*10000;
+    b = (n/1000) - ((n%1000)/1000);
+    n = n - b*1000;
+    c = (n/100) - ((n%100)/100);
+    n = n - c*100;
+    d = (n/10) - ((n%10)/10);
+    e = n - d*10;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << e << std::endl;
     return 0;
 }
