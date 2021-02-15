d809e0427335061d180dddd5352f20e2cba59b86
Baranovskij <70894805+Baranovskij@users.noreply.github.com>
2021-02-15 16:18:41 +0300

    add solution

--- 11_lab/main.cpp
+++ 11_lab/main.cpp
@@ -2,7 +2,18 @@
 // минимальное значения, которые можно записать в переменную типа int. 
 
 #include <iostream>
-
 int main() {
     // Решение тут
+    int i;
+    int j;
+    i = 1;
+    j = -1;
+    while (i > 0)
+        i = i+1;
+    while (j < 0)
+        j = j - 1;
+    std::cout << i << std::endl;
+    std::cout << j << std::endl;
+
+    return(0);
 }
