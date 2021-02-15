91c8b7a2891f81f7a27f8a4a21139ba379d45157
Baranovskij <70894805+Baranovskij@users.noreply.github.com>
2021-02-15 17:06:04 +0300

    add right solution

--- 10_lab/main.cpp
+++ 10_lab/main.cpp
@@ -2,18 +2,14 @@
 // с клавиатуры пользователем.
 
 #include <iostream>
-
+int absolute_value(x){
+    if (x < 0) 
+        x = -x;
+    return x;
+}    
 int main() {
     std::cout << "Enter a number: ";
-    int a;
-    std::cin >> a;
-    int absolute_value;
-
-    if (a >= 0)
-        absolute_value = a;
-    else
-        absolute_value = -a;
-
-    std::cout << absolute_value << std::endl;
-    return(0);
+    std::cin >> x;
+    std::cout << absolute_value(x) << std::endl;
+    
 }
