de6a80effa7902f8d4d80dbc5cae77e46cac6b0e
Efremenkof <71060779+Efremenkof@users.noreply.github.com>
2021-03-01 11:44:02 +0300

    Update main.cpp

--- 16_lab/main.cpp
+++ 16_lab/main.cpp
@@ -1,6 +1,18 @@
 // Допишите программу, которая вычисляет расстояние Хэмминга бинарных представлений двух целых положительных чисел. 
 
 #include <iostream>
+    int hamming(int a, int b)
+    {
+        int ham=0;
+        while (a>0 || b>0)
+        {
+            if(a%2!=b%2)
+                ham++;
+            a=a/2;
+            b=b/2;
+        }
+        return ham;
+    }
 
 int main() {
     std::cout << hamming(7, 3) << std::endl;
