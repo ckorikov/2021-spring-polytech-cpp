e79ba5cc9ddc018a9295d5d4d1a1d86ed0dc1f37
Efremenkof <71060779+Efremenkof@users.noreply.github.com>
2021-03-01 16:48:02 +0300

    Update main.cpp

--- 15_lab/main.cpp
+++ 15_lab/main.cpp
@@ -1,12 +1,15 @@
-// Вывести представление целого десятичного числа N в системе счисления с основанием M. M и N вводятся пользователем с клавиатуры.
+// Вывести представление целого десятичного числа N в системе счисления с основанием M. M и N вводятся пользователем с
+// клавиатуры.
 // Для примера посмотрите задачу 14
 
 #include <iostream>
 
 int main() {
-    int N, M, number=0, mul=1;
-
-    std::cin>>N>>M;
+    int N, M;
+    std::cout<<"Enter the number"<<std::endl;
+    std::cin>>N;
+    std::cout<<"Enter the numbering system"<<std::endl;
+    std::cin>>M;
     if(M==1)
     {
         for(int i=0; i<N; i++)
@@ -15,12 +18,18 @@ int main() {
         }
         return 0;
     }
+    std::cout<<"Inverse number: ";
     while(N>0) {
         int system = N % M;
-        N = N / M;
-        number = number + system * mul;
-        mul = mul * 10;
+        N /= M;
+        if(system<10)
+        {
+            std::cout<<system;
+        } else
+        {
+            std::cout<<static_cast<char>(system+'a'-10);
+        }
     }
-    std::cout << number << std::endl;
+    std::cout<<std::endl;
     return 0;
 }
