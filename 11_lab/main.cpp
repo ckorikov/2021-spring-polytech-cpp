363b03449f2afe770d45ed39f941ff0f38c13b0e
TimmiJK <max.bagaev@mail.ru>
2021-03-01 03:15:36 +0300

    Add 09, 10 and 11 labs

--- 11_lab/main.cpp
+++ 11_lab/main.cpp
@@ -4,5 +4,11 @@
 #include <iostream>
 
 int main() {
-    // Решение тут
+    int max_int = 0;
+    int i;
+    for (i = 1; i > max_int; i++) {
+        max_int = max_int + 1;
+    }
+    std::cout << "Value of the max int: " << max_int << std::endl;
+    std::cout << "Value of the min int: " << max_int + 1 << std::endl;
 }
