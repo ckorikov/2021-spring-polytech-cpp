8c42d2ffa08f5d8509dfcebcebddcb0be634c8ca
TimmiJK <max.bagaev@mail.ru>
2021-03-17 01:55:09 +0300

    Add 18 lab

--- 18_lab/main.cpp
+++ 18_lab/main.cpp
@@ -4,56 +4,87 @@
 
 #include <cassert>
 
-enum Command {
+enum class Command {
     Add,
     Sub,
     Mul,
     Div
 };
 
-double calc(int operation, double x, double y) {
+double calc(enum class Command operation, double x, double y) {
     double result;
-    if (operation == 0) {
-        result = x + y;
-    } else if (operation == 1) {
-        result = x - y;
-    } else if (operation == 2) {
-        result = x * y;
-    } else if (operation == 3) {
-        if (y != 0) {
-            result = x / y;
-        } else {
-            result = -1;
-        }
-    } else {
-        result = 0;
+    switch (operation) {
+        case Command::Add:
+            result = x + y;
+            break;
+        case Command::Sub:
+            result = x - y;
+            break;
+        case Command::Mul:
+            result = x * y;
+            break;
+        case Command::Div:
+            if (y != 0) {
+                result = x / y;
+            }
+            break;
+        default:
+            result = 0;
+            break;
     }
     return result;
 }
 
 int main() {
+    // Операции с нулями
     assert(calc(Command::Add, 0.0, 0.0) == 0.0);
     assert(calc(Command::Sub, 0.0, 0.0) == 0.0);
     assert(calc(Command::Mul, 0.0, 0.0) == 0.0);
+    assert(calc(Command::Div, 0.0, 5.0) == 0.0);
 
     // Коммутативность сложения
-    assert(calc(Command::Add, 2.0, 1.0) == 3.0);
-    assert(calc(Command::Add, 1.0, 2.0) == 3.0);
-    // Коммутативность умножения
-    assert(calc(Command::Mul, 2.0, 8.0) == 16.0);
-    assert(calc(Command::Mul, 8.0, 2.0) == 16.0);
+    assert(calc(Command::Add, 2.0, 1.0) == calc(Command::Add, 1.0, 2.0));
 
-    assert(calc(Command::Div, 3.0, 5.0) == 0.6);
-    assert(calc(Command::Div, 4.0, 2.0) == 2.0);
-    assert(calc(Command::Div, 0.0, 5.0) == 0.0);
-    assert(calc(Command::Div, 5.0, 0.0) == -1);
-    assert(calc(Command::Div, -2.41, 2) == -1.205);
-    assert(calc(Command::Add, 5.0, -9.0) == -4.0);
-    assert(calc(Command::Add, 1.0, 6.89) == 7.89);
-    assert(calc(Command::Sub, 1.0, -9.0) == 10.0);
-    assert(calc(Command::Sub, 1.0, 9.0) == -8.0);
-    assert(calc(Command::Sub, 2.45, 9.0) == -6.55);
-    assert(calc(Command::Mul, -1.5, -9.0) == 13.5);
+    // Ассоциативность сложения
+    assert(calc(Command::Add, calc(Command::Add, 2.0, 3.0), 5.0) ==
+           calc(Command::Add, calc(Command::Add, 5.0, 3.0), 2.0));
+
+    // Коммутативность умножения
+    assert(calc(Command::Mul, 2.0, 8.0) == calc(Command::Mul, 8.0, 2.0));
+
+    // Ассоциативность умножения
+    assert(calc(Command::Mul, calc(Command::Mul, 3.0, 4.0), 2.0) ==
+           calc(Command::Mul, calc(Command::Mul, 2.0, 4.0), 3.0));
+
+    // Дистрибутивность умножения
+    assert(calc(Command::Mul, calc(Command::Add, 3.0, 4.0), 5.0) ==
+           calc(Command::Add, calc(Command::Mul, 3.0, 5.0), calc(Command::Mul, 4.0, 5.0)));
+
+    // Операции с проверкой знаков
+    // + +
+    // - -
+    // + -
+    // - +
+    // Сложение
+    assert(calc(Command::Add, 3.0, 5.0) == 8.0);
+    assert(calc(Command::Add, -3.0, -5.0) == -8.0);
+    assert(calc(Command::Add, -3.0, 5.0) == 2.0);
+    assert(calc(Command::Add, 3.0, -5.0) == -2.0);
+    // Вычитание
+    assert(calc(Command::Sub, 2.0, 1.0) == 1.0);
+    assert(calc(Command::Sub, -2.0, -1.0) == -1.0);
+    assert(calc(Command::Sub, 2.0, -1.0) == 3.0);
+    assert(calc(Command::Sub, -2.0, 1.0) == -3.0);
+    // Умножение
+    assert(calc(Command::Mul, 5.0, 5.0) == 25.0);
+    assert(calc(Command::Mul, -5.0, -5.0) == 25.0);
+    assert(calc(Command::Mul, 5.0, -5.0) == -25.0);
+    assert(calc(Command::Mul, -5.0, 5.0) == -25.0);
+    // Деление
+    assert(calc(Command::Div, 10.0, 5.0) == 2.0);
+    assert(calc(Command::Div, -10.0, -5.0) == 2.0);
+    assert(calc(Command::Div, 10.0, -5.0) == -2.0);
+    assert(calc(Command::Div, -10.0, 5.0) == -2.0);
 
     return 0;
 }
