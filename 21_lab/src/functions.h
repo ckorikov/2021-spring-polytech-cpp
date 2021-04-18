274ebc1fc2e124265edd02f7fba3527df00482ef
NovoselovVadim <vadimnovosiolov@yandex.ru>
2021-04-18 17:20:46 +0300

    Add Homework

--- 21_lab/src/functions.h
+++ 21_lab/src/functions.h
@@ -2,5 +2,10 @@
 #define FUNCTIONS_H
 
 // Объявления функций тут
+int Max_number(int number[], int length);
+
+int Min_number(int number[], int length);
+
+int Number_repetition(int number[], int length, int target);
 
 #endif // FUNCTIONS_H
