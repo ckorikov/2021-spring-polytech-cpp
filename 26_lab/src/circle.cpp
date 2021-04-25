87b44ffba1675af2a08e6f9f42cfd46665aa9597
Andrey Zykov <andrei-z30@yandex.ru>
2021-04-25 22:56:39 +0300

    Lab 26 Ver_0.9 correct circle

--- 26_lab/src/circle.cpp
+++ 26_lab/src/circle.cpp
@@ -1,25 +1,18 @@
 #include "circle.h"
 
 void Circle::draw() {
-    char **const arr = createArr(2 * radius + 1, 2 * radius + 1);
+    char **const arr = createArr(2 * radius, 2 * radius);
 
-//    for (int x = 0; x <= 2 * radius; ++x) {
-//        float y = sqrt(radius * radius - ((x - radius) * (x - radius)));
-//        draftLine(arr, radius, radius, x, radius - y);
-//        draftLine(arr, radius, radius, x, radius + y);
-//    }
-
-    for (int i = 0; i <= 2 * radius; ++i) {
-        for (int j = 0; j <= 2 * radius; ++j) {
-            float x = std::abs(radius - j);
-            float y = std::abs(radius - i);
-            if (radius * radius >= (x * x + y * y)) {
+    for (int i = 0; i < 2 * radius; ++i) {
+        for (int j = 0; j < 2 * radius; ++j) {
+            float x = std::abs(radius - 0.5 - j);
+            float y = std::abs(radius - 0.5 - i);
+            if ((radius - 0.5) * (radius - 0.5) >= (x * x + y * y)) {
                 arr[i][j] = '*';
             }
         }
     }
-
-    outArr(arr,2 * radius + 1, 2 * radius + 1);
+    outArr(arr,2 * radius, 2 * radius);
 }
 
 void Circle::SetRadius(float r) {
